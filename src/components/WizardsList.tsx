309075931cb025461e2a34949cce109be6536f97
Pavel Sadlon <p.sadlon13@gmail.com>
2022-08-19 01:03:40 +0200

    css for list

--- src/components/WizardsList.tsx
+++ src/components/WizardsList.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import WizardService from "src/services/WizardService";
 import Wizard from "src/types/Wizard";
+import "../assets/style.css";
 
 const WizardsList: React.FC = () => {
   const [wizards, setWizards] = useState<Wizard[]>([]);
@@ -21,18 +22,45 @@ const WizardsList: React.FC = () => {
   };
 
   return (
-    <div className="list row">
-      <div className="col-md-6">
-        <h4>Wizards List</h4>
-        <ul className="list-group">
-          {wizards.map((wizard: Wizard, index: number) => (
-            <li key={index}>
-              <a href={"/wizard/" + wizard.id}>
-                {wizard.firstName} {wizard.lastName}
-              </a>
-            </li>
-          ))}
-        </ul>
+    <div className="container mt-3 mb-4">
+      <div className="col-lg-9 mt-4 mt-lg-0">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="user-dashboard-info-box table-responsive mb-0 bg-white p-4 shadow-sm">
+              <table className="table manage-candidates-top mb-0">
+                <thead>
+                  <tr>
+                    <th>Wizards</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="candidates-list">
+                    {wizards.map((wizard: Wizard, index: number) => (
+                      <td className="title" key={index}>
+                        <div className="thumb">
+                          <img
+                            className="img-fluid"
+                            src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                            alt=""
+                          />
+                        </div>
+                        <div className="candidate-list-details">
+                          <div className="candidate-list-info">
+                            <div className="candidate-list-title">
+                              <a href={"/wizard/" + wizard.id}>
+                                {wizard.firstName} {wizard.lastName}
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
